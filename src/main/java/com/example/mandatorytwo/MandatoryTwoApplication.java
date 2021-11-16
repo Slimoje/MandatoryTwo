c7fd6056474e27be4cb86e20c5b4ed580371ffb0
ADMIN <martin.steen.c@gmail.com>
2021-11-16 13:56:14 +0100

    asa

---
+++ src/main/java/com/example/mandatorytwo/MandatoryTwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mandatorytwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MandatoryTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MandatoryTwoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mandatorytwo/MandatoryTwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mandatorytwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MandatoryTwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
