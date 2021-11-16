3375134cd4d3338eaac42b0fa0a05ed97fb76ca8
Mads Møller <67467953+MadsMoeller@users.noreply.github.com>
2021-11-16 16:24:13 +0100

    Added "Contestant"-class. Though, no foreign keys added.

---
+++ src/main/java/com/example/mandatorytwo/models/Contestant.java
@@ -0,0 +1,28 @@
+package com.example.mandatorytwo.models;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Table(name = "Contestants")
+@Entity
+public class Contestant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+
+    @Column
+    private int kills;
+
+    @Column
+    private int deaths;
+
+    @Column
+    private Enum team;
+
+    @Column
+    private Enum status;
+}
