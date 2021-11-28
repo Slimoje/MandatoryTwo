dae7fae48024625b500ca3a5fdcc584527a507b1
Mads Møller <67467953+MadsMoeller@users.noreply.github.com>
2021-11-28 22:28:43 +0100

    Worked some more on that damned match.... made functionality to update the contestants of a match

--- src/main/resources/templates/matches/match.js
+++ src/main/resources/templates/matches/match.js
@@ -41,19 +41,62 @@ function addContestantToTeam(contestant){
 
 function createContestantCard(contestant, teamDivElement){
     const contestantCardElement = document.createElement("div");
+    contestantCardElement.id = "contestant-card-id-"+contestant.id;
     contestantCardElement.classList.add("contestant-card");
     contestantCardElement.innerHTML =
+
         `
         <div class="champion-icon-div">
             <img class="champion-icon-image" src=${contestant.champion.image}>
         </div>
+        
         <div class="summoner-name-and-level">${contestant.summoner.summonerName} - lvl. ${contestant.summoner.summonerLevel}</div>
+        
         <div class="champion-name">${contestant.champion.name}</div>
         <div class="team-role">${contestant.teamRole}</div>
         <div class="k-d-a">K/D/A</div>
-        <div class="k-d-a-value">${contestant.kills}/${contestant.deaths}/${contestant.assists}</div> 
+        <div class="k-d-a-value">
+            <span>${contestant.kills}</span>
+            /
+            <span>${contestant.deaths}</span>
+            /
+            <span>${contestant.assists}</span>
+        </div> 
         `
+
+         /*
+        `
+        <div class="summoner-name-and-level">
+            <input id="summoners-list-id-${contestant.id}" list="summoners-datalist-id-${contestant.id}" value="${contestant.summoner.summonerName}">
+            <datalist id="summoners-datalist-id-${contestant.id}" class="summoners-list"></datalist>
+        </div>
+        
+        <div class="champion-name">
+            <input id="champions-list-id-${contestant.id}" list="champions-datalist-id-${contestant.id}" value="${contestant.champion.name}">
+            <datalist id="champions-datalist-id-${contestant.id}" class="champions-list"></datalist>    
+        </div>
+        
+        <div class="team-role">
+            <select id="team-role-select" value="${contestant.teamRole}">
+                <option value="TOP">TOP</option>
+                <option value="MID">MID</option>
+                <option value="BOTTOM">BOTTOM</option>
+                <option value="JUNGLER">JUNGLER</option>
+                <option value="SUPPORT">SUPPORT</option>
+            </select>
+        </div>
+        <div class="k-d-a">K/D/A</div>
+        <div class="k-d-a-value">
+            <span><input id="kills-input-${contestant.id}" class="KDA-input-field" type="number" value="${contestant.kills}"></span>
+            /
+            <span><input id="deaths-input-${contestant.id}" class="KDA-input-field" type="number" value="${contestant.deaths}"></span>
+            /
+            <span><input id="assists-input-${contestant.id}" class="KDA-input-field" type="number" value="${contestant.assists}"></span>
+        </div> 
+        `
+
+         */
     teamDivElement.appendChild(contestantCardElement);
 }
 
-document.getElementById("edit-match-button").addEventListener("click", () => console.log("Edit this match"))
+document.getElementById("edit-match-button").addEventListener("click", () => updateMatch(matchToView))
--- src/main/resources/templates/matches/updateMatch.js
+++ src/main/resources/templates/matches/updateMatch.js
@@ -1,16 +1,70 @@
-const summoners = [];
-const champions = [];
+let summonersList;
+let championsList;
+let summonerOptions = '';
+let championOptions = '';
 
 fetch(baseURL+"/summoners")
     .then(response => response.json())
-    .then(summoner => summoners.push(summoner))
-    .then(console.log(summoners));
+    .then(summoners => {
+        summonersList = summoners;
+        for (let i = 0; i < summonersList.length; i++) {
+            summonerOptions += '<option value="' + summonersList[i].summonerName + '">\n';
+        }
+    });
 
 fetch(baseURL+"/champions")
     .then(response => response.json())
-    .then(champion => champions.push(champion))
-    .then(console.log(champions));
+    .then(champions => {
+        championsList = champions;
+        for (let i = 0; i < championsList.length; i++) {
+            championOptions += '<option value="' + championsList[i].name + '">\n';
+        }
+    });
 
-function updateMatch(){
+
+
+function updateMatch(matchToUpdate){
+    console.log(matchToUpdate);
+    matchToUpdate.contestants.map(updateContestantCard);
+
+}
+
+function updateContestantCard(contestant){
+    console.log("update contestant with id:", contestant.id);
+    const contestantCardToUpdate = document.getElementById("contestant-card-id-"+contestant.id);
+
+    contestantCardToUpdate.innerHTML =
+        `
+        <div class="summoner-name-and-level">
+            <input id="summoners-list-id-${contestant.id}" list="summoners-datalist-id-${contestant.id}" value="${contestant.summoner.summonerName}">
+            <datalist id="summoners-datalist-id-${contestant.id}" class="summoners-list"></datalist>
+        </div>
+        
+        <div class="champion-name">
+            <input id="champions-list-id-${contestant.id}" list="champions-datalist-id-${contestant.id}" value="${contestant.champion.name}">
+            <datalist id="champions-datalist-id-${contestant.id}" class="champions-list"></datalist>    
+        </div>
+        
+        <div class="team-role">
+            <select id="team-role-select" placeholder="${contestant.teamRole}">
+                <option value="TOP">TOP</option>
+                <option value="MID">MID</option>
+                <option value="BOTTOM">BOTTOM</option>
+                <option value="JUNGLER">JUNGLER</option>
+                <option value="SUPPORT">SUPPORT</option>
+            </select>
+        </div>
+        <div class="k-d-a">K/D/A</div>
+        <div class="k-d-a-value">
+            <span><input id="kills-input-${contestant.id}" class="KDA-input-field" type="number" value="${contestant.kills}"></span>
+            /
+            <span><input id="deaths-input-${contestant.id}" class="KDA-input-field" type="number" value="${contestant.deaths}"></span>
+            /
+            <span><input id="assists-input-${contestant.id}" class="KDA-input-field" type="number" value="${contestant.assists}"></span>
+        </div> 
+        `
+    document.getElementById("summoners-datalist-id-"+contestant.id).innerHTML = summonerOptions;
+    document.getElementById("champions-datalist-id-"+contestant.id).innerHTML = championOptions;
+
 
 }
